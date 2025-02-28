b375baeffc36d594b7c8f16aa6ed161391621707
Asiya Ikhlaq <ikhlaqasiya7@gmail.com>
2025-02-28 03:42:42 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var nam = "Asiya";
+var fath = "Ikhlaq";
+var total = nam+" " +fath ;
+alert(total)
+
+ var a= +prompt("enter  a number")
+ var b =+prompt("enter  a number")
+ var c=+prompt("enter  a number")
+ var d=+prompt("enter  a number")
+ 
+document.write( a +b+ c+ d)
